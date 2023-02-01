859873ea1bac23e7325940869aac58be2f352ae0
Vilsivul <92055005+VLadislav-Davidenko@users.noreply.github.com>
2023-02-01 18:09:45 +0200

    Adding CRUD Functionality

--- base/views.py
+++ base/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Q
 from .models import Advocate
 from .serializers import AdvocateSerializer
 
@@ -14,15 +15,43 @@ def getRoutes(request):
     return Response(routes)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def advocate_list(request):
-    advocates = Advocate.objects.all()
-    serializer = AdvocateSerializer(advocates, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        query = request.GET.get('query')
+        if query == None:
+            query = ''
+        advocates = Advocate.objects.filter(
+            Q(username__icontains=query) |
+            Q(bio__icontains=query)
+        )
+        serializer = AdvocateSerializer(advocates, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        advocate = Advocate.objects.create(
+        username = request.data["username"],
+        bio = request.data["bio"]
+        )
+        serializer = AdvocateSerializer(advocate)
+        return Response(serializer.data)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT', 'DELETE'])
 def advocate_details(request, username):
     advocate = Advocate.objects.get(username=username)
-    serializer = AdvocateSerializer(advocate, many=False)
-    return Response(serializer.data)
+
+    if request.method == 'GET':
+        serializer = AdvocateSerializer(advocate, many=False)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        advocate.username = request.data["username"]
+        advocate.bio = request.data["bio"]
+
+        advocate.save()
+        serializer = AdvocateSerializer(advocate, many = False)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        advocate.delete()
+        return Response("User was deleted")
